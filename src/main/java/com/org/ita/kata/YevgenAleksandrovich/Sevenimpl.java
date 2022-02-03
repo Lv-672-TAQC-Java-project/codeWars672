491b195647617647b29eb77c7215c0e74de958b9
YevgenAleksandrovich <l.jegoush@gmail.com>
2022-02-04 01:58:59 +0200

    init project

---
+++ src/main/java/com/org/ita/kata/YevgenAleksandrovich/Sevenimpl.java
@@ -0,0 +1,20 @@
+package com.org.ita.kata.YevgenAleksandrovich;
+
+import com.org.ita.kata.Seven;
+
+public class Sevenimpl implements Seven {
+    @Override
+    public long newAvg(double[] arr, double navg) {
+        return 0;
+    }
+
+    @Override
+    public String seriesSum(int n) {
+        return null;
+    }
+
+    @Override
+    public int whereIsHe(int p, int bef, int aft) {
+        return 0;
+    }
+}
