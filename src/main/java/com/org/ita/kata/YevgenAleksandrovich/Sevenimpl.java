6fc8af8cc4aab3b9c58a6d40a1c1ba53f471d268
YevgenAleksandrovich <l.jegoush@gmail.com>
2022-02-05 02:00:54 +0200

    temp

--- src/main/java/com/org/ita/kata/YevgenAleksandrovich/Sevenimpl.java
+++ src/main/java/com/org/ita/kata/YevgenAleksandrovich/Sevenimpl.java
@@ -3,9 +3,16 @@ package com.org.ita.kata.YevgenAleksandrovich;
 import com.org.ita.kata.Seven;
 
 public class Sevenimpl implements Seven {
+    /*Benefactor*/
     @Override
     public long newAvg(double[] arr, double navg) {
-        return 0;
+        double sum = 0;
+        for (double i:arr) {
+            sum+=i;
+        }
+        long result = (long) Math.ceil(navg*(arr.length+1)-sum);
+        if (result<0) throw new IllegalArgumentException();
+        return result;
     }
 
     @Override
