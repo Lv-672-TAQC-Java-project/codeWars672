931ecf6c71a1cabaf7811f7d284e23b09f32e158
YevgenAleksandrovich <l.jegoush@gmail.com>
2022-02-06 00:21:53 +0200

    temp

--- src/main/java/com/org/ita/kata/YevgenAleksandrovich/Sevenimpl.java
+++ src/main/java/com/org/ita/kata/YevgenAleksandrovich/Sevenimpl.java
@@ -7,10 +7,20 @@ public class Sevenimpl implements Seven {
     public long newAvg(double[] arr, double navg) {
         return 0;
     }
-
+  /*Sum of the first nth term of Series*/
     @Override
     public String seriesSum(int n) {
-        return null;
+        int counter = 4;
+        double sum=1;
+
+        for (int i = 1; i < n; i++) {
+            sum+=(double) 1/counter;
+            counter+=3;
+
+        }
+        double scale = Math.pow(10, 2);
+
+        return String.valueOf(Math.round(sum * scale) / scale);
     }
 
     @Override
