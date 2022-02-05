90ff193287cf1d0a83128a7f9574bd78fe16ca1e
yurii <yuriidru23@gmail.com>
2022-02-05 18:14:47 +0200

    Changed divisibleBy method

--- src/main/java/com/org/ita/kata/implementation/YuriiDruzhytskyi/EightImpl.java
+++ src/main/java/com/org/ita/kata/implementation/YuriiDruzhytskyi/EightImpl.java
@@ -45,6 +45,22 @@ public class EightImpl implements Eight {
 
     @Override
     public int[] divisibleBy(int[] numbers, int divider) {
-        return new int[0];
+        int count = 0;
+        for (int i = 0; i < numbers.length; i++) {
+            if (numbers[i] % divider == 0) {
+                count++;
+            }
+        }
+
+        int[] arrayFin = new int[count];
+        int x = 0;
+        for (int i = 0; i < numbers.length; i++) {
+            if (numbers[i] % divider == 0) {
+                arrayFin[x] = numbers[i];
+                x++;
+            }
+        }
+
+        return arrayFin;
     }
 }
