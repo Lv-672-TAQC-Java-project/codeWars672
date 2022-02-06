d4a0b0e0c67c3a27924e6c0aa02e35df3828cd43
Veglok <Qwertywo2000@gmail.com>
2022-02-06 22:51:16 +0200

    152. added 'twoDecimalPlaces' method body

--- src/main/java/com/org/ita/kata/implementation/RomanVytrykush/EightImpl.java
+++ src/main/java/com/org/ita/kata/implementation/RomanVytrykush/EightImpl.java
@@ -41,7 +41,7 @@ public class EightImpl implements Eight {
 
     @Override
     public double twoDecimalPlaces(double number) {
-        return 0;
+        return Math.round(number * 100) / 100.0;
     }
 
     @Override
