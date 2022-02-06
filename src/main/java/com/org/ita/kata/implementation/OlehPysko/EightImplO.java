1d7dc71d4be1f9361f14fe3d9b64cbf232b14aaf
OlehPysko <pysko.oleh@gmail.com>
2022-02-06 12:09:06 +0200

    94

--- src/main/java/com/org/ita/kata/implementation/OlehPysko/EightImplO.java
+++ src/main/java/com/org/ita/kata/implementation/OlehPysko/EightImplO.java
@@ -30,7 +30,10 @@ public class EightImplO implements Eight {
 
     @Override
     public int[] squareOrSquareRoot(int[] array) {
-        return new int[0];
+
+        for (int i = 0; i < array.length; i++) {
+            if (Math.sqrt(array[i]) % 1 == 0) array[i] = (int) Math.sqrt(array[i]);
+            else array[i] = array[i] * array[i];
     }
 
     @Override
