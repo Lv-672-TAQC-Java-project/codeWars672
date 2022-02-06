6086f0c840cd766a7c63e60e031ffaf0974d48a1
OlehPysko <pysko.oleh@gmail.com>
2022-02-06 15:50:02 +0200

    111

--- src/main/java/com/org/ita/kata/implementation/OlehPysko/SevenImplO.java
+++ src/main/java/com/org/ita/kata/implementation/OlehPysko/SevenImplO.java
@@ -5,7 +5,15 @@ import com.org.ita.kata.Seven;
 public class SevenImplO implements Seven {
     @Override
     public long newAvg(double[] arr, double navg) {
-        return 0;
+        double suma = 0;
+        for (int i = 0; i < arr.length; i++) {
+            suma += arr[i];
+        }
+
+        long avereg = (long) Math.ceil((arr.length + 1) * navg - suma);
+
+        if (avereg < 0) throw new IllegalArgumentException();
+        return avereg;
     }
 
     @Override
