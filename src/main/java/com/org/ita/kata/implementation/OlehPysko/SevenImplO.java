ebe1c5d05c107f038bb474004f3e01aa330aeda4
OlehPysko <pysko.oleh@gmail.com>
2022-02-06 16:39:50 +0200

    122

--- src/main/java/com/org/ita/kata/implementation/OlehPysko/SevenImplO.java
+++ src/main/java/com/org/ita/kata/implementation/OlehPysko/SevenImplO.java
@@ -18,7 +18,15 @@ public class SevenImplO implements Seven {
 
     @Override
     public String seriesSum(int n) {
-        return null;
+
+        double sum = 0.0;
+
+        for (int i=0; i<n; i++) {
+            sum = sum + 1.0 / (1 + 3 * i);
+        }
+        //double sum2 = 1.0 + sum;
+        String strResult = String.format("%.2f", sum);
+        return strResult;
     }
 
     @Override
