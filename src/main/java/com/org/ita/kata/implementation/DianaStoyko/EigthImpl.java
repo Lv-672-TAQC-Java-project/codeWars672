efbb4f71e222581f021f0167d605fe4af2314f37
DianaSt123 <diana.stoyko@gmail.com>
2022-02-07 11:57:27 +0200

    195

--- src/main/java/com/org/ita/kata/implementation/DianaStoyko/EigthImpl.java
+++ src/main/java/com/org/ita/kata/implementation/DianaStoyko/EigthImpl.java
@@ -32,7 +32,13 @@ public class EigthImpl implements Eight {
 
     @Override
     public int[] countPositivesSumNegatives(int[] input) {
-        return new int[0];
+        if (input == null || input.length == 0) return new int[] {};
+        int count = 0,sum = 0;
+        for (int i : input) {
+            if (i > 0) count ++;
+            if (i < 0) sum += i;
+        }
+        return new int[] {count,sum};
     }
 
     @Override
