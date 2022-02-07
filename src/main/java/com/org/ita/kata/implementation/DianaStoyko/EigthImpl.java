82394a08463e840a25c9fa56d923b0fd0cacdae9
DianaSt123 <diana.stoyko@gmail.com>
2022-02-07 12:07:34 +0200

    201

--- src/main/java/com/org/ita/kata/implementation/DianaStoyko/EigthImpl.java
+++ src/main/java/com/org/ita/kata/implementation/DianaStoyko/EigthImpl.java
@@ -48,7 +48,7 @@ public class EigthImpl implements Eight {
 
     @Override
     public boolean amIWilson(double n) {
-        return false;
+        return n == 5 || n == 13 || n == 563;
     }
 
     @Override
