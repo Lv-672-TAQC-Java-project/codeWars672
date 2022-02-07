74dc8221a67e1f78d22a2488a7fe59c5d8ee5907
YevgenAleksandrovich <l.jegoush@gmail.com>
2022-02-07 23:51:49 +0200

    temp

--- src/main/java/com/org/ita/kata/YevgenAleksandrovich/Fiveimpl.java
+++ src/main/java/com/org/ita/kata/YevgenAleksandrovich/Fiveimpl.java
@@ -5,9 +5,55 @@ import com.org.ita.kata.Five;
 import java.math.BigInteger;
 
 public class Fiveimpl implements Five {
+
+    /*African Rain*/
     @Override
     public int artificialRain(int[] v) {
-        return 0;
+        int[] arrRight = countRight(v);
+        int[] arrLeft = countLeft(v);
+        int count = 0;
+
+        for (int i = 0; i < arrLeft.length; i++) {
+            count = Math.max(arrLeft[i] + arrRight[i] + 1, count);
+        }
+        return count;
+    }
+
+    static boolean goLeft(int[] numbers, int i) {
+        if (i == 0)
+            return false;
+        return numbers[i - 1] <= numbers[i];
+    }
+
+    static boolean goRight(int[] numbers, int i) {
+        if (numbers.length - 1 == i)
+            return false;
+        return numbers[i + 1] <= numbers[i];
+    }
+
+    static int[] countLeft(int[] numbers) {
+        int[] count = new int[numbers.length];
+
+        for (int i = 0; i < numbers.length; i++) {
+            if (goLeft(numbers, i)) {
+                count[i] = count[i - 1] + 1;
+            } else {
+                count[i] = 0;
+            }
+        }
+        return count;
+    }
+
+    static int[] countRight(int[] numbers) {
+        int[] count = new int[numbers.length];
+        for (int i = numbers.length - 1; i >= 0; i--) {
+            if (goRight(numbers, i)) {
+                count[i] = count[i + 1] + 1;
+            } else {
+                count[i] = 0;
+            }
+        }
+        return count;
     }
 
     @Override
