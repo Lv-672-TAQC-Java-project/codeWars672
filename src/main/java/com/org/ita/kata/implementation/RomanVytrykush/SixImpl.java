17b46da4d3667acebb1e7f70f9ec3fa5da0f2019
Veglok <Qwertywo2000@gmail.com>
2022-02-07 23:39:16 +0200

    221. added 'balance' method body in SixImpl

--- src/main/java/com/org/ita/kata/implementation/RomanVytrykush/SixImpl.java
+++ src/main/java/com/org/ita/kata/implementation/RomanVytrykush/SixImpl.java
@@ -10,7 +10,28 @@ public class SixImpl implements Six {
 
     @Override
     public String balance(String book) {
-        return null;
+        double inTotal = 0;
+        int counter = 0;
+
+        String newBook = book.replaceAll("[^a-zA-Z0-9 .\n]", "");
+        newBook = newBook.trim().replaceAll(" +", " ");
+        String [] strArr = newBook.split("[\\n]+");
+        double prevBalance = Double.parseDouble(strArr[0]);
+        StringBuilder newBalance = new StringBuilder();
+        newBalance.append("Original Balance: " + strArr[0]);
+
+        for (int i = 1; i < strArr.length; i++) {
+            String[] splitln = strArr[i].split("[ ]+");
+            prevBalance = prevBalance - Double.parseDouble(splitln[2]);
+            inTotal = inTotal + Double.parseDouble(splitln[2]);
+            String inter = String.format("\\r\\n%s %s %s Balance %.2f", splitln[0], splitln[1], splitln[2], prevBalance);
+            counter++;
+
+            newBalance.append(inter);
+        }
+        newBalance = newBalance.append(String.format
+                ("\\r\\nTotal expense  %.2f\\r\\nAverage expense  %.2f", inTotal, inTotal / counter));
+        return newBalance.toString();
     }
 
     @Override
