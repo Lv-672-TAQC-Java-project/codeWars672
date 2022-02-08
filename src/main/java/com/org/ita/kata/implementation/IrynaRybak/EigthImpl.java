bd6571e44ac6c0597aa390901eb8e4c3857c8fe6
Ira Rybak <ira.rybak17@gmail.com>
2022-02-08 18:39:58 +0200

    added squareOrSquareRoot body in EigthImpl

--- src/main/java/com/org/ita/kata/implementation/IrynaRybak/EigthImpl.java
+++ src/main/java/com/org/ita/kata/implementation/IrynaRybak/EigthImpl.java
@@ -20,7 +20,15 @@ public class EigthImpl implements Eight {
 
     @Override
     public int[] squareOrSquareRoot(int[] array) {
-        return new int[0];
+        int[] arr = new int[array.length];
+        for (int i = 0; i < array.length; i++) {
+            if (Math.sqrt(array[i]) % 1 == 0) {
+                arr[i] = (int) Math.sqrt(array[i]);
+            } else {
+                arr[i] = (int) Math.pow(array[i], 2);
+            }
+        }
+        return arr;
     }
 
     @Override
