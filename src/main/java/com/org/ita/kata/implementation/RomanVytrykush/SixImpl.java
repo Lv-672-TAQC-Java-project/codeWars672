f76b66cd030db253095f8d8b53e24eeba462714c
Veglok <Qwertywo2000@gmail.com>
2022-02-09 00:52:15 +0200

    321. added 'stockSummary' method body in SixImpl

--- src/main/java/com/org/ita/kata/implementation/RomanVytrykush/SixImpl.java
+++ src/main/java/com/org/ita/kata/implementation/RomanVytrykush/SixImpl.java
@@ -2,6 +2,9 @@ package com.org.ita.kata.implementation.RomanVytrykush;
 
 import com.org.ita.kata.Six;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class SixImpl implements Six {
     @Override
     public long findNb(long m) {
@@ -22,7 +25,6 @@ public class SixImpl implements Six {
     public double mean(String town, String strng) {
         return 0;
     }
-
     @Override
     public double variance(String town, String strng) {
         return 0;
@@ -35,6 +37,22 @@ public class SixImpl implements Six {
 
     @Override
     public String stockSummary(String[] lstOfArt, String[] lstOf1stLetter) {
-        return null;
+        if (lstOfArt.length == 0 || lstOf1stLetter.length == 0) {
+            return "";
+        }
+        HashMap<String, Integer> artMap = new HashMap<>();
+
+        for (String list : lstOfArt) {
+            String[] art = list.split(" ");
+            String firstLetter = art[0].substring(0, 1);
+            int quantity = Integer.parseInt(art[1]);
+
+            artMap.put(firstLetter, artMap.getOrDefault(firstLetter, 0) + quantity);
+        }
+        for (int i = 0; i < lstOf1stLetter.length; i++) {
+            Integer sumQuantity = artMap.get(lstOf1stLetter[i]) == null ? 0 : artMap.get(lstOf1stLetter[i]);
+            lstOf1stLetter[i] = "(" + lstOf1stLetter[i] + " : " + sumQuantity + ")";
+        }
+        return String.join(" - ", lstOf1stLetter);
     }
 }
