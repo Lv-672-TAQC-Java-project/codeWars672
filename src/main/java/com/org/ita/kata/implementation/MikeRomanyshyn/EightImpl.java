5b7022e961578c2db425087a19d52861333f1e71
Michael Romanyshyn <mikesunry@gmail.com>
2022-02-08 10:44:31 +0200

    add new branch 18

--- src/main/java/com/org/ita/kata/implementation/MikeRomanyshyn/EightImpl.java
+++ src/main/java/com/org/ita/kata/implementation/MikeRomanyshyn/EightImpl.java
@@ -2,30 +2,56 @@ package com.org.ita.kata.implementation.MikeRomanyshyn;
 
 import com.org.ita.kata.Eight;
 
+import java.sql.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+
 public class EightImpl implements Eight {
     @Override
     public int liters(double time) {
-        return 0;
+        return (int)(time * 0.5);
     }
 
     @Override
     public double getVolumeOfCuboid(double length, double width, double height) {
-        return 0;
+        return (length * width * height);
     }
 
     @Override
     public float mpgToKPM(float mpg) {
-        return 0;
+        double result = (mpg * 1.609344) / 4.54609188;
+        return Float.parseFloat(String.format("%.2f", result));
     }
 
     @Override
     public int[] squareOrSquareRoot(int[] array) {
-        return new int[0];
+        int[] newArray = new int[array.length];
+
+        for (int i = 0; i < array.length; i++) {
+            if (Math.sqrt(array[i]) % 1 == 0) {
+                newArray[i] = (int) Math.sqrt(array[i]);
+            } else {
+                newArray[i] = (int) Math.pow(array[i], 2);
+            }
+        }
+
+        return newArray;
     }
 
     @Override
     public int[] countPositivesSumNegatives(int[] input) {
-        return new int[0];
+        if ((input == null) || (input.length == 0)) { return new int[0]; }
+
+        int counter = 0;
+        int negativeTemp = 0;
+
+        for (int i : input) {
+            if (i > 0) counter++;
+            if (i < 0) negativeTemp += i;
+        }
+
+        return new int[] {counter, negativeTemp};
     }
 
     @Override
