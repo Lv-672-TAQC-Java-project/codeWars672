2a47a0b32b60c6783ea0a525132f7c124cc8e3f3
DianaSt123 <diana.stoyko@gmail.com>
2022-02-09 03:51:43 +0200

    207

--- src/main/java/com/org/ita/kata/implementation/DianaStoyko/SevenImpl.java
+++ src/main/java/com/org/ita/kata/implementation/DianaStoyko/SevenImpl.java
@@ -5,7 +5,11 @@ import com.org.ita.kata.Seven;
 public class SevenImpl implements Seven {
     @Override
     public long newAvg(double[] arr, double navg) {
-        return 0;
+        double sum = 0;
+        for (double i: arr) sum += i;
+        long res = (long) Math.ceil((arr.length + 1) * navg - sum);
+        if (res < 0) throw new IllegalArgumentException();
+        return res;
     }
 
     @Override
