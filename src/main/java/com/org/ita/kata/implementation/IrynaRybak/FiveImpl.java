2d592da1192d3322cd80cb3bcf0cdcd214ccdf4c
Ira Rybak <ira.rybak17@gmail.com>
2022-02-11 11:04:00 +0200

    added zeros body in FiveImpl

--- src/main/java/com/org/ita/kata/implementation/IrynaRybak/FiveImpl.java
+++ src/main/java/com/org/ita/kata/implementation/IrynaRybak/FiveImpl.java
@@ -17,7 +17,31 @@ public class FiveImpl implements Five {
 
     @Override
     public int zeros(int n) {
-        return 0;
+        if(n > 25){
+            System.out.println("The number is too big ( > 25)");
+            return -1;
+        }
+        long number = fastorial(n);
+        String str = Long.toString(number);
+        int count = 0;
+        for (int i = 0; i < str.length(); i++) {
+            if (number % 10 == 0) {
+                count = count + 1;
+                number = number / 10;
+            } else {
+                break;
+            }
+        }
+        return count;
+    }
+
+    public static long fastorial(int n) {
+        long result = 1L;
+        for (long i = 2; i <= n; i++) {
+            result *= i;
+        }
+        System.out.println(result);
+        return result;
     }
 
     @Override
