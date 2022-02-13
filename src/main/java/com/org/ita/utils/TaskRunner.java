7df238790c7515adf29d328b1843f09168afa721
Ira Rybak <ira.rybak17@gmail.com>
2022-02-13 17:20:40 +0200

    Implement method runTaskSmallest

--- src/main/java/com/org/ita/utils/TaskRunner.java
+++ src/main/java/com/org/ita/utils/TaskRunner.java
@@ -1,5 +1,7 @@
 package com.org.ita.utils;
 
+import java.util.Arrays;
+
 public class TaskRunner {
     private Users user ;
     private ConsoleScanner consoleScanner;
@@ -15,5 +17,13 @@ public class TaskRunner {
         double input = consoleScanner.readDouble();
         int result = user.getImplEight().liters(input);
         System.out.println(String.format("Result: %d", result));
+    }
+
+    public void runTaskSmallest (){
+        System.out.println("Run task Smallest");
+        System.out.println("Enter a long number: ");
+        long input =  consoleScanner.readLong();
+        long[] result = user.getImplFive().smallest(input);
+        System.out.println(String.format("Result: %s", Arrays.toString(result)));
     }
 }
