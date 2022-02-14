9c79b1053800b8cdb49b1e3b9e2301d4d3544caa
Michael Romanyshyn <mikesunry@gmail.com>
2022-02-14 15:54:48 +0200

    add default case to Menu

--- src/main/java/com/org/ita/utils/Menu.java
+++ src/main/java/com/org/ita/utils/Menu.java
@@ -20,39 +20,35 @@ public class Menu {
             System.out.println("Please make your choice:");
 
             choose = consoleScanner.readInt();
-
-            if ((choose >= 0) && (choose <= 4)) {
-
-                switch (choose) {
-                    case 1:
-                        System.out.println("List of implementations : ");
-                        Users.printAllUserName();
-                        break;
-                    case 2:
-                        System.out.println("List of tasks : ");
-                        Tasks.printAllTaskName();
-                        break;
-                    case 3:
-                        System.out.println("Please choose a user by id : ");
-                        choose = consoleScanner.readInt();
-                        user = Users.getById(choose);
-                        System.out.println("Your user is : " + user.getName());
-                        break;
-                    case 4:
-                        System.out.println("Please choose a task by id : ");
-                        choose = consoleScanner.readInt();
-                        if (user != null) {
-                            new TaskRunner(user).runTask(choose);
-                        } else {
-                            System.out.println("Please, firstly choose a user. Press 3 !");
-                        }
-                        break;
-                    case 0:
-                        return;
-                }
-
-            } else {
-                System.out.println("Incorrect! Please make your choice from 1 till 4 and 0 to exit.");
+            switch (choose) {
+                case 1:
+                    System.out.println("List of implementations : ");
+                    Users.printAllUserName();
+                    break;
+                case 2:
+                    System.out.println("List of tasks : ");
+                    Tasks.printAllTaskName();
+                    break;
+                case 3:
+                    System.out.println("Please choose a user by id : ");
+                    choose = consoleScanner.readInt();
+                    user = Users.getById(choose);
+                    System.out.println("Your user is : " + user.getName());
+                    break;
+                case 4:
+                    System.out.println("Please choose a task by id : ");
+                    choose = consoleScanner.readInt();
+                    if (user != null) {
+                        new TaskRunner(user).runTask(choose);
+                    } else {
+                        System.out.println("Please, firstly choose a user. Press 3 !");
+                    }
+                    break;
+                case 0:
+                    return;
+                default:
+                    System.out.println("Incorrect! Please make your choice from 1 till 4 and 0 to exit.");
+                    break;
             }
         }
     }
