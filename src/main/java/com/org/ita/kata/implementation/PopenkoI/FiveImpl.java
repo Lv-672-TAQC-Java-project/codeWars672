6f94284305841f3b071bd1feea1f9b9223394cc0
Inna <innapopenko45@gmail.com>
2022-02-17 12:38:47 +0200

    added impl solveSum

--- src/main/java/com/org/ita/kata/implementation/PopenkoI/FiveImpl.java
+++ src/main/java/com/org/ita/kata/implementation/PopenkoI/FiveImpl.java
@@ -88,7 +88,7 @@ public class FiveImpl implements Five {
 
     @Override
     public double solveSum(double m) {
-        return 0;
+        return 1 + ((1 - Math.sqrt(4 * m + 1)) / (2 * m));
     }
 
     @Override
