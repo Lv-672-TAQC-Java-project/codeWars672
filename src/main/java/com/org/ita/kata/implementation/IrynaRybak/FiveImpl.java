311c9963f3ecf9fbb77c6c9ab1c941b652863806
Ira Rybak <ira.rybak17@gmail.com>
2022-03-18 17:06:22 +0200

    fix zeros and factorial

--- src/main/java/com/org/ita/kata/implementation/IrynaRybak/FiveImpl.java
+++ src/main/java/com/org/ita/kata/implementation/IrynaRybak/FiveImpl.java
@@ -17,15 +17,6 @@ public class FiveImpl extends Base implements Five {
         return true;
     }
 
-    public static long fastorial(int n) {
-        long result = 1L;
-        for (long i = 2; i <= n; i++) {
-            result *= i;
-        }
-        System.out.println(result);
-        return result;
-    }
-
     @Override
     public int artificialRain(int[] v) {
         return 0;
@@ -51,22 +42,24 @@ public class FiveImpl extends Base implements Five {
 
     @Override
     public int zeros(int n) {
-        if (n > 25) {
-            System.out.println("The number is too big ( > 25)");
-            return -1;
-        }
-        long number = fastorial(n);
-        String str = Long.toString(number);
+        String numbers = factorial(n).toString();
         int count = 0;
-        for (int i = 0; i < str.length(); i++) {
-            if (number % 10 == 0) {
+        for (int i = numbers.length() - 1; i > 0; i--) {
+            if (numbers.charAt(i) == '0') {
                 count = count + 1;
-                number = number / 10;
             } else {
                 break;
             }
         }
         return count;
+    }
+
+    public static BigInteger factorial(int t) {
+        BigInteger result = BigInteger.ONE;
+        for (int i = 2; i <= t; i++) {
+            result = result.multiply(BigInteger.valueOf(i));
+        }
+        return result;
     }
 
     @Override
